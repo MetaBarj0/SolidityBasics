6124e8b3dc6ff7be348eb550acc174a7340f6209
MetaBarj0 <troctsch.cpp@gmail.com>
2025-01-24 14:27:17 +0000

    fix: rewrite of NFT implementation
    No minting, no test so far

--- ERC721/NFT.sol
+++ ERC721/NFT.sol
@@ -2,79 +2,96 @@
 pragma solidity 0.8.28;
 
 import "./SimplifiedERC721.sol";
-import "./NFTOptional.sol";
 
-contract NFT is SimplifiedERC721, NFTOptional {
-  address public owner;
+contract NFT is SimplifiedERC721 {
+  string public name;
+  string public symbol;
+  uint8 public totalSupply;
 
   constructor() {
-    owner = msg.sender;
+    _owner = msg.sender;
+    name = "Useless NFT";
+    symbol = "UNFT";
     totalSupply = 10;
   }
 
   error InvalidZeroAddress();
   error InvalidTokenId();
-  error InvalidTokenOwner();
-  error NotOwnerOrOperator();
+  error NotOwnerNorOperatorNorApproved();
+  error NotOwner();
 
-  function balanceOf(address _owner) external view override returns (uint256) {
-    return _balances[_owner];
+  function balanceOf(address owner) external view override returns (uint256) {
+    require(owner != address(0), InvalidZeroAddress());
+
+    return _balances[owner];
   }
 
-  function ownerOf(uint256 _tokenId) external view override returns (address) {
-    return _ownedTokens[_tokenId];
+  function ownerOf(uint256 tokenId) external view override returns (address) {
+    address owner = _ownedTokens[tokenId];
+
+    if (owner == address(0)) revert InvalidZeroAddress();
+
+    return owner;
   }
 
-  function transferFrom(address _from, address _to, uint256 _tokenId) external payable override {
-    require(_to != address(0), InvalidZeroAddress());
-    require(_ownedTokens[_tokenId] != address(0), InvalidTokenId());
-    require(_isTokenOwner(_tokenId), InvalidTokenOwner());
+  function transferFrom(address from, address to, uint256 tokenId) external payable override {
+    require(to != address(0) && from != address(0), InvalidZeroAddress());
+    require(_ownedTokens[tokenId] == from, NotOwner());
+    require(_ownedTokens[tokenId] != address(0), InvalidTokenId());
+    require(_senderIsOwnerOrOperatorOrApproved(from, tokenId), NotOwnerNorOperatorNorApproved());
 
-    _ownedTokens[_tokenId] = _to;
+    delete _tokenApprovals[from];
+    delete _approvalTokens[tokenId];
 
-    emit Transfer(_from, _to, _tokenId);
+    _ownedTokens[tokenId] = to;
+    emit Transfer(from, to, tokenId);
   }
 
-  function approve(address _approved, uint256 _tokenId) external payable override {
-    _tokenApprovals[_approved] = _tokenId;
-    _approvalTokens[_tokenId] = _approved;
+  function approve(address approved, uint256 tokenId) external payable override {
+    require(
+      _senderIsOwnerOrOperatorOrApproved(approved, tokenId),
+      NotOwnerNorOperatorNorApproved()
+    );
 
-    emit Approval(_ownedTokens[_tokenId], _approved, _tokenId);
+    _tokenApprovals[approved] = tokenId;
+    _approvalTokens[tokenId] = approved;
+
+    emit Approval(_ownedTokens[tokenId], approved, tokenId);
   }
 
-  function setApprovalForAll(address _operator, bool _approved) external override {
-    require(_operator != address(0), InvalidZeroAddress());
-    require(_isTokenOwnerOrOperator(), NotOwnerOrOperator());
+  function setApprovalForAll(address operator, bool approved) external override {
+    require(operator != address(0), InvalidZeroAddress());
 
-    _approvedOperators[_operator] = _approved;
+    _approvedOperators[msg.sender][operator] = approved;
 
-    emit ApprovalForAll(msg.sender, _operator, _approved);
+    emit ApprovalForAll(msg.sender, operator, approved);
   }
 
-  function getApproved(uint256 _tokenId) external view returns (address) {
-    return _approvalTokens[_tokenId];
+  function getApproved(uint256 tokenId) external view returns (address) {
+    require(_ownedTokens[tokenId] != address(0), InvalidTokenId());
+
+    return _approvalTokens[tokenId];
   }
 
-  function isApprovedForAll(
-    address _owner,
-    address _operator
-  ) external view override returns (bool) {}
+  function isApprovedForAll(address owner, address operator) external view override returns (bool) {
+    return _approvedOperators[owner][operator];
+  }
 
   mapping(address owner => uint256 amount) private _balances;
   mapping(uint256 tokenId => address owner) private _ownedTokens;
-  mapping(address approved => uint256 tokenId) private _tokenApprovals;
   mapping(uint256 tokenId => address approved) private _approvalTokens;
-  mapping(address operator => bool) _approvedOperators;
+  mapping(address approved => uint256 tokenId) private _tokenApprovals;
+  mapping(address owner => mapping(address operator => bool approved)) private _approvedOperators;
 
-  function _isTokenOwner(uint256 _tokenId) private view returns (bool) {
+  address private _owner;
+
+  function _senderIsOwnerOrOperatorOrApproved(
+    address from,
+    uint256 tokenId
+  ) private view returns (bool) {
     return
-      msg.sender == owner ||
-      msg.sender == _approvalTokens[_tokenId] ||
-      msg.sender == _ownedTokens[_tokenId] ||
-      _approvedOperators[msg.sender];
-  }
-
-  function _isTokenOwnerOrOperator() private view returns (bool) {
-    return msg.sender == owner || _approvedOperators[msg.sender];
+      msg.sender == _owner ||
+      _approvedOperators[from][msg.sender] ||
+      _approvalTokens[tokenId] == msg.sender;
   }
 }
