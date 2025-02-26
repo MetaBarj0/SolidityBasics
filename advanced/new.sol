8f60e8c9e2dac753d9c375fbd5df029f95979e8c
MetaBarj0 <troctsch.cpp@gmail.com>
2025-02-26 09:34:21 +0000

    fix: new

--- advanced/new.sol
+++ advanced/new.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
+pragma solidity 0.8.28;
 
 // how to create a contract instance from another contract
 // Briefly talking about create and create2 (?)
@@ -21,10 +21,11 @@ contract Account {
 contract AccountFactory {
   Account[] public accounts;
 
-  function create(address owner) external {
+  function create(address owner) external payable {
     // deploy the contract
     Account account = new Account{value: 42}(owner);
 
+    // deployed contract accessible through the accounts state variable
     accounts.push(account);
   }
 }
